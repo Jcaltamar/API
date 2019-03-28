bf012b087ae8df938b13c8919fdf560841fc3a78
Jcaltamare <Jcaltamare@gmail.com>
2019-03-28 09:33:42 -0500

    add alias

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('register', 'Auth\RegisterController@register');
+    Route::post('login','Auth\LoginController@login');
+});
+
+
