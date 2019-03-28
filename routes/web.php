aca89b2af8f8bf6185eb60793dd146c6fd0dd22e
Jcaltamare <Jcaltamare@gmail.com>
2019-03-28 00:56:34 -0500

    add alias

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
 
 Route::get('/', function () {
     return view('welcome');
