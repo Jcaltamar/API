b0e27dd5b817ee64c19effd9cae334e6222e50b7
Jcaltamare <Jcaltamare@gmail.com>
2019-03-28 10:09:07 -0500

    add alias

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('register', 'Auth\RegisterController@register');
-    Route::post('login','Auth\LoginController@login');
+    Route::post('login','API\Auth\LoginController@login');
 });
 
 
